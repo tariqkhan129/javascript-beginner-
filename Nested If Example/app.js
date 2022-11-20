6008b37d6492d090b1dfc813e1bda6f81dace7d8
Tariq <tariqcs129@gmail.com>
2022-11-20 15:24:48 +0500

    nested if login example

---
+++ Nested If Example/app.js
@@ -0,0 +1,22 @@
+function nestedif (){
+
+    let dbEmail="abc@example.com";
+    let dbPassword = "12345";
+
+    let inputEmail= document.querySelector("#email").value;
+    let inputPassword= document.querySelector("#password").value;
+
+    if (inputEmail===dbEmail)
+    {
+        if (inputPassword===dbPassword)
+        {
+            document.querySelector("#result").innerHTML=` Login Successful`;
+        }
+            else {
+            document.querySelector("#result").innerHTML=` Incoorect Password <br>  Enter Password again`;
+            }}
+     else
+     {
+        document.querySelector("#result").innerHTML=` Invalid Email <br>  enter a Valid Email`;
+     }       
+    }
