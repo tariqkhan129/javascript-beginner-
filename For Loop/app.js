9a272cdf965351071b5dc9b526bdcbaacbab97d8
Tariq <tariqcs129@gmail.com>
2022-12-17 22:26:18 +0500

    display numbers using for loop

---
+++ For Loop/app.js
@@ -0,0 +1,9 @@
+
+
+    function PrintNumber(){
+      let z = "";
+    for (let y = 0; y < 11; y++){
+      z = z + " Number " + y + " is printed. <br>";
+    }
+    document.querySelector("#loop").innerHTML=z;
+    }
